09d12133a668af9d552dd1864fc485f609c5e23e
Roman <51263203+Romanov09@users.noreply.github.com>
2023-04-17 20:10:28 +0300

    Update.java

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -3,45 +3,46 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) throws IOException {
-
+        System.out.println("Введите выражение: ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        calc(input);
+    }
+    public static String calc(String input) throws IOException {
         Convert converter = new Convert();
         String[] signs = {"+", "-", "/", "*"};
         String[] checkSigns = {"\\+", "-", "/", "\\*"};
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите выражение: ");
-        String exp = sc.nextLine();
-
         int signsIndex = -1;
         for (int i = 0; i < signs.length; i++) {
-            if(exp.contains(signs[i])){
+            if (input.contains(signs[i])) {
                 signsIndex = i;
                 break;
             }
         }
 
-        if(signsIndex == -1){
+        if (signsIndex == -1) {
             throw new IOException("Строка не является математической операцией!");
         }
 
-        String[] data = exp.split(checkSigns[signsIndex]);
-        if (!(data.length ==2)) {
+        String[] data = input.split(checkSigns[signsIndex]);
+        if (!(data.length == 2)) {
             throw new IOException("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         }
 
-        if(converter.isRoman(data[0]) == converter.isRoman(data[1])){
-            int a,b;
+        if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
+            int a, b;
             boolean isRoman = converter.isRoman(data[0]);
 
-            if(isRoman){
+            if (isRoman) {
                 a = converter.romanToInt(data[0]);
                 b = converter.romanToInt(data[1]);
-                if(a>10 || b>10) {
+                if (a > 10 || b > 10) {
                     throw new IOException("Калькулятор должен принимать на вход числа от 1 до 10 включительно, не более!");
                 }
-            }else{
+            } else {
                 a = Integer.parseInt(data[0]);
                 b = Integer.parseInt(data[1]);
-                if(a>10||a<1 || b>10||b<1) {
+                if (a > 10 || a < 1 || b > 10 || b < 1) {
                     throw new IOException("Калькулятор должен принимать на вход числа от 1 до 10 включительно, не более!");
                 }
             }
@@ -53,17 +54,17 @@ public class Calculator {
                 default -> a / b;
             };
 
-            if(isRoman){
-                if(result <= 0) {
+            if (isRoman) {
+                if (result <= 0) {
                     throw new IOException("В римской системе нет отрицательных чисел!");
                 }
                 System.out.println(converter.intToRoman(result));
-            }
-            else{
+            } else {
                 System.out.println("Ответ: " + result);
             }
-        }else{
+        } else {
             throw new IOException("Используются одновременно разные системы счисления!");
         }
+        return input;
     }
 }
